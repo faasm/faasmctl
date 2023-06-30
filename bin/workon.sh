ac64b199e23923e3b88296cc3cebf486908b6c40
Carlos Segarra <carlos@carlossegarra.com>
2023-06-30 15:24:31 +0000

    bin: add scripts to initialise venv

---
+++ bin/workon.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" >/dev/null 2>&1 && pwd )"
+PROJ_ROOT="${THIS_DIR}/.."
+pushd ${PROJ_ROOT}>>/dev/null
+
+# ----------------------------
+# Virtualenv
+# ----------------------------
+
+export VENV_PATH="${PROJ_ROOT}/venv-faasmctl"
+
+if [ ! -d ${VENV_PATH} ]; then
+    ${PROJ_ROOT}/bin/create_venv.sh
+fi
+
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+source ${VENV_PATH}/bin/activate
+
+# ----------------------------
+# Invoke tab-completion
+# (http://docs.pyinvoke.org/en/stable/invoke.html#shell-tab-completion)
+# ----------------------------
+
+_complete_invoke() {
+    local candidates
+    candidates=`invoke --complete -- ${COMP_WORDS[*]}`
+    COMPREPLY=( $(compgen -W "${candidates}" -- $2) )
+}
+
+# If running from zsh, run autoload for tab completion
+if [ "$(ps -o comm= -p $$)" = "zsh" ]; then
+    autoload bashcompinit
+    bashcompinit
+fi
+complete -F _complete_invoke -o default invoke inv
+
+# ----------------------------
+# Environment vars
+# ----------------------------
+
+VERSION_FILE=${PROJ_ROOT}/VERSION
+export FAASMCTL_VERSION=$(cat ${VERSION_FILE})
+
+export PS1="(faasmctl-dev) $PS1"
+
+# -----------------------------
+# Splash
+# -----------------------------
+
+echo ""
+echo "----------------------------------"
+echo "FaasmCTL Dev Virtual Env."
+echo "Version: ${FAASMCTL_VERSION}"
+echo "Project root: $(pwd)"
+echo "----------------------------------"
+echo ""
+
+popd >> /dev/null
