868b5f7a834f2ec3f46093e57b88bc90ad97c4c6
Carlos Segarra <carlos@carlossegarra.com>
2023-07-06 16:26:43 +0000

    tasks: add task to tag code and bump code version

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,9 @@
 from invoke import Collection
 
 from . import format_code
+from . import git
 
 ns = Collection(
     format_code,
+    git,
 )
---
+++ tasks/git.py
@@ -0,0 +1,57 @@
+from invoke import task
+from faasmctl.util.env import PROJ_ROOT
+from faasmctl.util.version import get_version
+from subprocess import run
+
+VERSIONED_FILES = [
+    "pyproject.toml",
+    "README",
+    "VERSION",
+]
+
+
+@task
+def tag(ctx, force=False):
+    """
+    Creates git tag from the current tree
+    """
+    version = get_version()
+    tag_name = "v{}".format(version)
+    run(
+        "git tag {} {}".format("--force" if force else "", tag_name),
+        shell=True,
+        check=True,
+        cwd=PROJ_ROOT,
+    )
+
+    run(
+        "git push {} origin {}".format("--force" if force else "", tag_name),
+        shell=True,
+        check=True,
+        cwd=PROJ_ROOT,
+    )
+
+
+@task
+def bump(ctx, patch=False, minor=False, major=False):
+    """
+    Bump the code version by --patch, --minor, or --major
+    """
+    old_ver = get_version()
+    new_ver_parts = old_ver.split(".")
+
+    if patch:
+        idx = 2
+    elif minor:
+        idx = 1
+    elif major:
+        idx = 0
+    else:
+        raise RuntimeError("Must set one in: --[patch,minor,major]")
+
+    new_ver_parts[idx] = str(int(new_ver_parts[idx]) + 1)
+    new_ver = ".".join(new_ver_parts)
+
+    for f in VERSIONED_FILES:
+        sed_cmd = "sed -i 's/{}/{}/g' {}".format(old_ver, new_ver, f)
+        run(sed_cmd, shell=True, check=True, cwd=PROJ_ROOT)
