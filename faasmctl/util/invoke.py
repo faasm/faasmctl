d128da86976f16cc476ed7c87baba17e972e41ef
Carlos <carlos@carlossegarra.com>
2023-07-17 21:10:34 +0100

    Function to upload shared files (#21)
    * upload: add method to upload file
    
    * gh: bump patch version
    
    * gh: acc. bump minor version to comply with semver
    
    * invoke: fixes after testing on experiments-mpi

--- faasmctl/util/invoke.py
+++ faasmctl/util/invoke.py
@@ -6,14 +6,25 @@ from faasmctl.util.config import (
 from faasmctl.util.docker import in_docker
 from faasmctl.util.gen_proto.faabric_pb2 import BatchExecuteRequestStatus
 from faasmctl.util.planner import prepare_planner_msg
-from google.protobuf.json_format import MessageToJson, Parse
+from google.protobuf.json_format import MessageToDict, MessageToJson, Parse
 from requests import post
 from time import sleep
 
 
-def invoke_wasm(msg_dict, num_messages=1, ini_file=None):
+def invoke_wasm(msg_dict, num_messages=1, dict_out=False, ini_file=None):
     """
     Main entrypoint to invoke an arbitrary message in a Faasm cluster
+
+    Arguments:
+    - msg_dict (dict): dict-like object to build a Message Protobuf from
+    - num_messages (int): number of said messages to include in the BER
+    - dict_out (bool): flag to indicate that we expect the result as a JSON
+                       instead than as a Message class
+    - ini_file (str): path to the cluster's INI file
+
+    Return:
+    - The BERStatus result either in a Protobuf class or as a dict if `dict_out`
+      is set
     """
     req = batch_exec_factory(msg_dict, num_messages)
     msg = prepare_planner_msg("EXECUTE_BATCH", MessageToJson(req, indent=None))
@@ -24,7 +35,16 @@ def invoke_wasm(msg_dict, num_messages=1, ini_file=None):
     host, port = get_faasm_planner_host_port(ini_file, in_docker())
     url = "http://{}:{}".format(host, port)
 
-    result = invoke_and_await(url, msg, num_messages)
+    # Work out the number of messages to expect in the result basing on the
+    # original message
+    expected_num_messages = num_messages
+    if "mpi_world_size" in msg_dict:
+        expected_num_messages = msg_dict["mpi_world_size"]
+
+    result = invoke_and_await(url, msg, expected_num_messages)
+
+    if dict_out:
+        return MessageToDict(result)
 
     return result
 
--- faasmctl/util/upload.py
+++ faasmctl/util/upload.py
@@ -32,3 +32,30 @@ def upload_python(func, python_file, ini_file=None):
 
     response = put(url, data=open(python_file, "rb"))
     print("Response ({}): {}".format(response.status_code, response.text))
+
+
+def upload_file(host_path, faasm_path, ini_file=None):
+    """
+    Upload a file to Faasm's distributed filesystem
+
+    This helper method uploads a file to Faasm's distributed filesystem for its
+    usage from WASM functions running in Faasm. A file uploaded to <faasm_path>
+    is visible from WASM functions at file path: faasm://<faasm_path>
+
+    Parameters:
+    - host_path (str): path in the host system to read the file from
+    - faasm_path (str): path in the Faasm file system to store the file to
+    - ini_file (str): path to the cluster's INI file
+    """
+    if not ini_file:
+        ini_file = get_faasm_ini_file()
+
+    host, port = get_faasm_upload_host_port(ini_file, in_docker())
+    url = "http://{}:{}/file".format(host, port)
+
+    response = put(
+        url,
+        data=open(host_path, "rb"),
+        headers={"FilePath": faasm_path},
+    )
+    print("Response ({}): {}".format(response.status_code, response.text))
