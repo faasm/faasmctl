91b4d90c5384f800fbdf1fc732600f4c9aa8de82
Carlos Segarra <carlos@carlossegarra.com>
2023-07-17 21:50:23 +0000

    fix typo

--- faasmctl/util/compose.py
+++ faasmctl/util/compose.py
@@ -179,7 +179,7 @@ def populate_host_sysroot(faasm_checkout, clean=False):
             # for the python functions. Overwriting all the sysroot with the
             # one tracked in the CPython image is unneccessary
             join("llvm-sysroot", "lib", "wasm32-wasi", "libpython3.8.a"),
-            join("llvm-sysroot", "lib", "include", "python3.8"),
+            join("llvm-sysroot", "include", "python3.8"),
         ],
     }
 
