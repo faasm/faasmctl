d713276e0d1d6ea3afaa67fbf8723bf4ba105279
Carlos Segarra <carlos@carlossegarra.com>
2023-07-19 16:17:43 +0000

    cli: add option to run a CLI against a k8s cluster

--- faasmctl/tasks/cli.py
+++ faasmctl/tasks/cli.py
@@ -1,10 +1,10 @@
-from faasmctl.util.backend import COMPOSE_BACKEND
 from faasmctl.util.compose import run_compose_cmd, wait_for_venv
 from faasmctl.util.config import (
     BACKEND_INI_STRING,
     get_faasm_ini_file,
     get_faasm_ini_value,
 )
+from faasmctl.util.docker import get_docker_tag
 from invoke import task
 from os.path import abspath
 
@@ -14,37 +14,61 @@ def do_run_cmd(cli, cmd, ini_file):
         ini_file = get_faasm_ini_file()
 
     backend = get_faasm_ini_value(ini_file, "Faasm", BACKEND_INI_STRING)
-    if backend != COMPOSE_BACKEND:
-        raise RuntimeError(
-            "Can only get a CLI shell for a cluster that is "
-            "running on a '{}' backend".format(COMPOSE_BACKEND)
-        )
+    if backend == "compose":
+        # To run a command in a `compose` backend, we fist start the CLI
+        # container in dettached mode (if it does not exist yet) and then we
+        # exec into it, copying the ini file
+        up_cmd = "up -d --no-recreate {}".format(cli)
+        run_compose_cmd(ini_file, up_cmd)
 
-    # First, start the container once to avoid recreating it multiple times
-    up_cmd = "up -d --no-recreate {}".format(cli)
-    run_compose_cmd(ini_file, up_cmd)
+        # Second, copy the the ini file inside the container
+        ini_file = abspath(ini_file)
+        ini_file_ctr_path = "/tmp/faasm.ini"
+        cp_cmd = "cp {} {}:{}".format(ini_file, cli, ini_file_ctr_path)
+        run_compose_cmd(ini_file, cp_cmd)
 
-    # Second, copy the the ini file inside the container
-    ini_file = abspath(ini_file)
-    ini_file_ctr_path = "/tmp/faasm.ini"
-    cp_cmd = "cp {} {}:{}".format(ini_file, cli, ini_file_ctr_path)
-    run_compose_cmd(ini_file, cp_cmd)
+        # Third, wait for the virtual environment to be ready if we need to. We
+        # only need to wait for the virtual environment if we are either
+        # mounting the source, or using one of the clients
+        wait_for_venv(ini_file, cli)
 
-    # Third, wait for the virtual environment to be ready if we need to. We
-    # only need to wait for the virtual environment if we are either mounting
-    # the source, or using one of the clients
-    wait_for_venv(ini_file, cli)
+        # Lastly, actually run the requested command
+        compose_cmd = [
+            "exec",
+            "-e FAASM_INI_FILE={}".format(ini_file_ctr_path),
+            "-it" if not cmd else "",
+            cli,
+            "bash" if not cmd else cmd,
+        ]
+        compose_cmd = " ".join(compose_cmd)
+        run_compose_cmd(ini_file, compose_cmd)
 
-    # Lastly, actually run the requested command
-    compose_cmd = [
-        "exec",
-        "-e FAASM_INI_FILE={}".format(ini_file_ctr_path),
-        "-it" if not cmd else "",
-        cli,
-        "bash" if not cmd else cmd,
-    ]
-    compose_cmd = " ".join(compose_cmd)
-    run_compose_cmd(ini_file, compose_cmd)
+    elif backend == "k8s":
+        # Using a CLI container with a cluster that runs on a `k8s` backend is
+        # only supported to upload functions from the CPP or Python container.
+        # As such we: (i) disable cli.faasm calls on a `k8s` backend, and (ii)
+        # use a standalone container to execute the command that we remove
+        # immediately after (without mounting any files)
+        ini_file_ctr_path = "/tmp/faasm.ini"
+        if cli == "cpp":
+            image_tag = get_docker_tag("CPP_CLI_IMAGE")
+        elif cli == "python":
+            image_tag = get_docker_tag("PYTHON_CLI_IMAGE")
+        else:
+            raise RuntimeError("Can't call cli.faasm on a `k8s` cluster!")
+
+        docker_cmd = [
+            "docker run --rm",
+            "-v {}:{}".format(ini_file, ini_file_ctr_path),
+            "-e FAASM_INI_FILE={}".format(ini_file_ctr_path),
+            "-it" if not cmd else "",
+            image_tag,
+            "bash" if not cmd else cmd,
+        ]
+        docker_cmd = " ".join(docker_cmd)
+
+    else:
+        raise RuntimeError("Unrecognised backend: {}".format(backend))
 
 
 @task
