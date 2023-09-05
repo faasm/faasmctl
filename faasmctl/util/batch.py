bff9aaaf965028d7ef988e8a98c54527c24a2a11
Carlos <carlos@carlossegarra.com>
2023-09-05 15:31:22 +0100

    Fixes for new Planner behaviour (#44)
    * faasm: bump tracked version
    
    * gh: bump minor version
    
    * ber: generate the latest proto files to generate valid BERs

--- faasmctl/util/batch.py
+++ faasmctl/util/batch.py
@@ -6,6 +6,8 @@ from faasmctl.util.random import generate_gid
 def batch_exec_factory(msg_dict, num_messages):
     req = BatchExecuteRequest()
     req.appId = generate_gid()
+    req.user = msg_dict["user"]
+    req.function = msg_dict["function"]
 
     for _ in range(num_messages):
         req.messages.append(message_factory(msg_dict, req.appId))
--- faasmctl/util/faasm.py
+++ faasmctl/util/faasm.py
@@ -1,6 +1,6 @@
 from os import environ
 
-FAASM_VERSION = "0.10.2"
+FAASM_VERSION = "0.11.0"
 
 
 def get_version():
