213e3e687ec60a7a9c296f7cd161e56ab01e6479
Carlos <carlos@carlossegarra.com>
2023-12-06 14:47:15 -0500

    invoke: add --cmdline and --mpi-world-size cmdline options (#52)

--- faasmctl/tasks/invoke.py
+++ faasmctl/tasks/invoke.py
@@ -3,16 +3,20 @@ from invoke import task
 
 
 @task(default=True)
-def invoke(ctx, user, func, ini_file=None):
+def invoke(ctx, user, func, ini_file=None, cmdline=None, mpi_world_size=None):
     """
     Invoke the execution of a user/func pair
 
     TODO: think how to enable all the possible command line values in a
     scalable way.
     """
-    # TODO: work out the number of messages
     num_messages = 1
     msg_dict = {"user": user, "function": func}
+
+    if cmdline is not None:
+        msg_dict["cmdline"] = cmdline
+    if mpi_world_size is not None:
+        msg_dict["mpi_world_size"] = int(mpi_world_size)
 
     # Invoke WASM using main API function
     result = invoke_wasm(msg_dict, num_messages, ini_file)
