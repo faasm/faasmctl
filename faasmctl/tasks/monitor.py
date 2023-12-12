61b48dd6a0721d76ec0851db59bc790f04af4247
Carlos <carlos@carlossegarra.com>
2023-12-12 13:35:14 +0000

    monitor: make ctr name unique so that we can monitor multiple clusters at the same time (#54)
    * monitor: make ctr name unique so that we can monitor multiple clusters at the same time
    
    * gh: bump minor code version

--- faasmctl/tasks/monitor.py
+++ faasmctl/tasks/monitor.py
@@ -1,4 +1,8 @@
-from faasmctl.util.config import get_faasm_ini_file, get_faasm_planner_host_port
+from faasmctl.util.config import (
+    get_faasm_ini_file,
+    get_faasm_ini_value,
+    get_faasm_planner_host_port,
+)
 from faasmctl.util.faasm import FAASM_CLI_IMAGE
 from faasmctl.util.gen_proto.planner_pb2 import GetInFlightAppsResponse
 from faasmctl.util.planner import get_available_hosts, prepare_planner_msg
@@ -10,11 +14,33 @@ from subprocess import run
 from sys import exit as sys_exit
 from time import sleep
 
-CTR_NAME = "faasm-is-migratable-workon"
+CTR_NAME_BASE = "faasm-is-migratable-workon"
+
+
+def get_ctr_name():
+    """
+    Generate a unique container name to be able to monitor multiple cluster
+    instances
+    """
+    backend = get_faasm_ini_value(get_faasm_ini_file(), "Faasm", "backend")
+
+    if backend == "compose":
+        return "{}-{}".format(
+            CTR_NAME_BASE,
+            get_faasm_ini_value(get_faasm_ini_file(), "Faasm", "cluster_name"),
+        )
+
+    # We assume that the planner IP will be unique across different k8s cluster
+    # instances (we replace the dots by dashes as we want to generate a suitable
+    # container name)
+    slug_planner_ip = get_faasm_ini_value(
+        get_faasm_ini_file(), "Faasm", "planner_host"
+    ).replace(".", "-", 3)
+    return "{}-{}".format(CTR_NAME_BASE, slug_planner_ip)
 
 
 def stop_container():
-    docker_cmd = "docker rm -f {}".format(CTR_NAME)
+    docker_cmd = "docker rm -f {}".format(get_ctr_name())
     out = run(docker_cmd, shell=True, capture_output=True)
     assert out.returncode == 0, "Error running docker (cmd: {}): {}".format(
         docker_cmd, out.stderr.decode("utf-8")
@@ -62,7 +88,7 @@ def get_apps_to_be_migrated(registered_workers, in_flight_apps, worker_occupatio
     docker_cmd = [
         "docker run -dt",
         "-v {}:{}".format(worker_occupation_file_path, worker_occupation_file_path),
-        "--name {}".format(CTR_NAME),
+        "--name {}".format(get_ctr_name()),
         FAASM_CLI_IMAGE,
     ]
     docker_cmd = " ".join(docker_cmd)
@@ -75,7 +101,7 @@ def get_apps_to_be_migrated(registered_workers, in_flight_apps, worker_occupatio
     for app in in_flight_apps.apps:
         docker_cmd = [
             "docker exec",
-            CTR_NAME,
+            get_ctr_name(),
             "bash -c '/build/faasm/bin/is_app_migratable {} {}'".format(
                 app.appId, worker_occupation_file_path
             ),
