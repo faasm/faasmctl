489f0510dd552391b0df941202e038f3cba89005
Carlos <carlos@carlossegarra.com>
2023-12-18 17:37:34 +0000

    invoke: support setting request's singleHost flag from command line (#58)
    * invoke: support setting request's singleHost flag from command line
    
    * gh: bump minor code version

--- faasmctl/util/batch.py
+++ faasmctl/util/batch.py
@@ -1,13 +1,12 @@
 from faasmctl.util.message import message_factory
 from faasmctl.util.gen_proto.faabric_pb2 import BatchExecuteRequest
 from faasmctl.util.random import generate_gid
+from google.protobuf.json_format import ParseDict
 
 
-def batch_exec_factory(msg_dict, num_messages):
-    req = BatchExecuteRequest()
+def batch_exec_factory(req_dict, msg_dict, num_messages):
+    req = ParseDict(req_dict, BatchExecuteRequest())
     req.appId = generate_gid()
-    req.user = msg_dict["user"]
-    req.function = msg_dict["function"]
 
     for _ in range(num_messages):
         req.messages.append(message_factory(msg_dict, req.appId))
