26f251d57ea44c952f4d496c0a95cef479f3c8a9
Carlos Segarra <carlos@carlossegarra.com>
2024-03-11 12:35:44 +0000

    faasmctl: fix python sysroot population

--- faasmctl/util/compose.py
+++ faasmctl/util/compose.py
@@ -256,8 +256,8 @@ def populate_host_sysroot(faasm_checkout, clean=False):
             # We are specific about which bits to copy from the llvm-sysroot
             # for the python functions. Overwriting all the sysroot with the
             # one tracked in the CPython image is unneccessary
-            join("llvm-sysroot", "lib", "wasm32-wasi", "libpython3.8.a"),
-            join("llvm-sysroot", "include", "python3.8"),
+            join("llvm-sysroot", "lib", "wasm32-wasi-threads", "libpython3.8.a"),
+            join("llvm-sysroot", "include", "wasm32-wasi-threads", "python3.8"),
         ],
     }
 
